2738119ffce9eba8adb69fd3e0cbd65d75a55177
Eugene Malchenko <nijoelite@gmail.com>
2019-06-13 15:44:48 +0300

    Use process.env in config

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  mongodb_uri: process.env.NODE_ENV === 'production'
-        ? process.env.MONGODB_URI
-        : 'mongodb://127.0.0.1:27017',
-  secret: 'abc',
+  mongodb_uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/testDatabase',
+  secret: process.env.secret || 'abc',
 };
